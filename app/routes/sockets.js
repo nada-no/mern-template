53c54e8882a91efa141b7d51b0cfb5afd4c54b5e
guest <guest@debian-BULLSEYE-live-builder-AMD64>
2022-04-07 16:11:45 +0200

    cambios de casa. Proyecto listo para empezar

--- app/routes/sockets.js
+++ app/routes/sockets.js
@@ -1,8 +1,13 @@
 var express = require("express"),
   router = express.Router(); 
 
+const user = require('../controllers/user');
+
 router.route("/").get(async (req, res, next) => {
   res.render("index");
 });
+
+
+router.post('/create', user.add);
 
 module.exports = router;
