58b0f973ae9542b01c6c7f8683ff35a49246cc94
nada <iawadancuestamolina@gmail.com>
2022-07-07 23:34:18 +0200

    react connected

--- server/app.js
+++ server/app.js
@@ -1,40 +1,35 @@
 require("dotenv").config();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 9000; //was 3000
 var express = require("express"),
-  app = express(),
-  server = require("http").createServer(app),
-  path = require("path");
-mongoose = require('mongoose');
-var session = require("express-session");
-app.use(session({ secret: 'keyboard cat', auth: false , resave: true, saveUninitialized: true}));
+	app = express(),
+	server = require("http").createServer(app),
+	path = require("path"),
+	mongoose = require("mongoose"),
+	cors = require("cors");
 
-app.use(function(req,res,next){
-  res.locals.session = req.session;
-  next();
-});
-
-
-
+//use CORS
+app.use(cors());
 
 server.listen(port, (err, res) => {
-  if (err) console.log(`ERROR: Connecting APP ${err}`);
-  else console.log(`Server is running on port ${port}`);
+	if (err) console.log(`ERROR: Connecting APP ${err}`);
+	else console.log(`Server is running on port ${port}`);
 });
 
 //creamos la base de datos
 mongoose.connect(
-  `mongodb://root:pass12345@mongodb-mern:27017/users?authSource=admin`,
-  { useNewUrlParser: true },
-  (err, res) => {
-    if (err) console.log(`ERROR: connecting to Database.  ${err}`);
-    else console.log("Successfully connect to MongoDB.");
-  }
+	`mongodb://root:pass12345@mongodb-mern:27017/users?authSource=admin`,
+	{ useNewUrlParser: true },
+	(err, res) => {
+		if (err) console.log(`ERROR: connecting to Database.  ${err}`);
+		else console.log("Successfully connect to MongoDB.");
+	}
 );
 
 // Import routes of our app
-var socketsRouter = require("./routes/router");
+var appRouter = require("./routes/router");
 var handlerError = require("./routes/handler");
+var apiRouter = require("./routes/APIroutes");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -44,8 +39,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define routes using URL path
-app.use("/", socketsRouter);
+// Define routes using URL path (order matters!)
+app.use("/", appRouter);
+app.use("/api", apiRouter);
 app.use(handlerError);
 
 module.exports = app;
